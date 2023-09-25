5ee6f31ffdc2a1b6c75ca3e320ef3a72a7e9c291
Colibri Shin <colibrishin@gmail.com>
2023-09-25 20:52:00 +0900

    split mesh renderer def and impl

--- Engine/EGMeshRenderer.hpp
+++ Engine/EGMeshRenderer.hpp
@@ -4,11 +4,8 @@
 #include "EGCommon.hpp"
 #include "EGComponent.hpp"
 #include "EGMesh.hpp"
-#include "EGShader.hpp"
-#include "EGTransform.hpp"
-#include "EGGameObject.hpp"
 
-namespace Engine
+namespace Engine::Component
 {
 	using namespace DirectX;
 
@@ -30,45 +27,4 @@ namespace Engine
 	private:
 		std::weak_ptr<Abstract::Mesh> mMesh{};
 	};
-
-	inline MeshRenderer::MeshRenderer(const Abstract::GameObject* owner)
-		: Component(L"MeshRenderer" + std::to_wstring(GetID()), Enums::COMPONENTTYPE::MESH, owner)
-	{
-	}
-
-	inline void MeshRenderer::Initialize()
-	{
-	}
-
-	inline void MeshRenderer::Update()
-	{
-	}
-
-	inline void MeshRenderer::FixedUpdate()
-	{
-	}
-
-	inline void MeshRenderer::Render()
-	{
-		if(const auto tr = GetOwner()->GetComponent<Abstract::Transform>().lock())
-		{
-			XMMATRIX view{};
-			XMMATRIX projection{};
-
-			Manager::SceneManager::GetActiveScene().lock()->GetSceneCoordination(view, projection);
-
-			XMMATRIX worldPos = SimpleMath::Matrix::CreateWorld(
-				tr->GetPosition(), DirectX::SimpleMath::Vector3::Forward, DirectX::SimpleMath::Vector3::Up);
-
-			const auto rotMat = SimpleMath::Matrix::CreateFromYawPitchRoll(tr->GetRotation());
-			XMMATRIX scaleMat = XMMatrixScalingFromVector(tr->GetScale());
-
-			worldPos = XMMatrixMultiply(worldPos, rotMat);
-
-			mMesh.lock()->Render(
-				worldPos,
-				view, 
-				projection);
-		}
-	}
 }
