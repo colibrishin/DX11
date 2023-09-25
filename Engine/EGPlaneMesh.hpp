7deaae368e451b813dd9244083a8b8944200bebc
Colibri Shin <colibrishin@gmail.com>
2023-09-25 20:54:20 +0900

    create bondingbox manually for default mesh

--- Engine/EGPlaneMesh.hpp
+++ Engine/EGPlaneMesh.hpp
@@ -9,17 +9,19 @@ namespace Engine::Mesh
 	class PlaneMesh : public Abstract::Mesh
 	{
 	public:
-		PlaneMesh(const std::wstring& key);
+		PlaneMesh(const std::wstring& key, const SimpleMath::Vector3& size);
 		PlaneMesh(const PlaneMesh& other) = default;
 		~PlaneMesh() override = default;
 
 		void Render(const XMMATRIX& world, const XMMATRIX& view, const XMMATRIX& proj) override;
+		void GetBoundingBox(BoundingBox& box) const override;
 
 	protected:
 		void SetParameter(ID3D11Device* device, ID3D11DeviceContext* ctx, const DirectX::CommonStates* state) override;
 
 	private:
 		std::unique_ptr<GeometricPrimitive> m_plane_;
+		SimpleMath::Vector3 m_size_;
 	};
 
 	inline void PlaneMesh::Render(const XMMATRIX& world, const XMMATRIX& view, const XMMATRIX& proj)
@@ -27,13 +29,20 @@ namespace Engine::Mesh
 		m_plane_->Draw(world, view, proj, Colors::White, nullptr, false, nullptr);
 	}
 
+	inline void PlaneMesh::GetBoundingBox(BoundingBox& box) const
+	{
+		BoundingBox::CreateFromPoints(box, +m_size_ / 2, -m_size_ / 2);
+	}
+
 	inline void PlaneMesh::SetParameter(ID3D11Device* device, ID3D11DeviceContext* ctx,
-		const DirectX::CommonStates* state)
+	                                    const DirectX::CommonStates* state)
 	{
 	}
 
-	inline PlaneMesh::PlaneMesh(const std::wstring& key): Mesh(std::to_wstring(GetID()) + L" PlaneMesh", key, "")
+	inline PlaneMesh::PlaneMesh(const std::wstring& key, const SimpleMath::Vector3& size): Mesh(std::to_wstring(GetID()) + L" PlaneMesh", key, "")
 	{
-		m_plane_ = GetDevice()->CreateBox({ 100.0f, 1.0f, 100.0f });
+		m_size_ = size;
+		m_size_.y = 1.0f;
+		m_plane_ = GetDevice()->CreateBox(m_size_);
 	}
 }
